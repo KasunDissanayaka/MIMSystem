7ee9432434b6f1228feb997d01199b06786e7f55
Kasun Dissanayaka <kasun.d@evicio.com>
2024-04-07 13:50:41 +0530

    API routs made for Madication module

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\MedicationInventory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,11 @@ Route::put('/v1/customers/{id}', [CustomerRecordController::class, 'update'])->m
 Route::delete('/v1/customers/{id}', [CustomerRecordController::class, 'destroy'])->middleware('auth:sanctum');
 Route::delete('/v1/customers/{id}/permanently', [CustomerRecordController::class, 'permanentlyDelete'])->middleware('auth:sanctum');
 Route::get('/v1/customers/{id}/restore', [CustomerRecordController::class, 'restore'])->middleware('auth:sanctum');
+
+Route::get('/v1/medication', [MedicationInventory::class, 'index'])->middleware('auth:sanctum');
+Route::post('/v1/medication', [MedicationInventory::class, 'store'])->middleware('auth:sanctum');
+Route::get('/v1/medication/{id}', [MedicationInventory::class, 'show'])->middleware('auth:sanctum');
+Route::put('/v1/medication/{id}', [MedicationInventory::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/v1/medication/{id}', [MedicationInventory::class, 'destroy'])->middleware('auth:sanctum');
+Route::delete('/v1/medication/{id}/permanently', [MedicationInventory::class, 'permanentlyDelete'])->middleware('auth:sanctum');
+Route::get('/v1/medication/{id}/restore', [MedicationInventory::class, 'restore'])->middleware('auth:sanctum');
